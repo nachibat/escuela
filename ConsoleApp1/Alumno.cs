080689f92d1e218524b52454bf2edb9bbb2eed68
Osvaldo Torrez <nachi2083@gmail.com>
2024-10-04 21:11:46 -0300

    Cambios en mostrar datos

--- ConsoleApp1/Alumno.cs
+++ ConsoleApp1/Alumno.cs
@@ -18,6 +18,7 @@ namespace ConsoleApp1
 
         public override void MostrarDatos()
         {
+            Console.Write("Datos importantes");
             Console.WriteLine("Legajo: {0} - Nombre completo: {1} {2} - Edad: {3} - Tiene libreta: {4}", this.Legajo, this.Nombre, this.Apellido, this.Edad, this.TieneLibreta);
         }
     }
